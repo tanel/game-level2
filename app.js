7ceb70308f74f869df9e037a3ed6dab96d7bc39b
Tanel Lebedev <tanel.lebedev@gmail.com>
2018-04-01 18:08:30 +0300

    Add audio input

--- app.js
+++ app.js
@@ -8,4 +8,15 @@ window.app.code = function (s) {
 
 
 window.onload = function () {
+	  var player = document.getElementById('player');
+
+	  var handleSuccess = function(stream) {
+	    if (window.URL) {
+	      player.src = window.URL.createObjectURL(stream);
+	    } else {
+	      player.src = stream;
+	    }
+	  };
+
+	  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(handleSuccess);
 };
